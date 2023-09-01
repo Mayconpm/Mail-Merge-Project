94dccf0f94fdca25dbf1a5c124275dcfa1aa8d12
Mayconpm <pm.maycon@gmail.com>
2023-09-01 04:44:48 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+starting_letter_file = "Input/Letters/starting_letter.txt"
+invited_names_file = "Input/Names/invited_names.txt"
+
+with open(starting_letter_file, "r") as file:
+    letter = file.read()
+
+with open(invited_names_file, "r") as file:
+    names = file.readlines()
+
+for name in names:
+    name = name.strip()
+    letter_temp = letter.replace("[name]", name)
+    with open(f"Output/ReadyToSend/{name}.txt", "w") as output_file:
+        output_file.write(letter_temp)
